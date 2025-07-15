438c13b8f2d9df2af29d428100f6ee9b0c93a1f6
iafy-io <ceo@iafy.io>
2025-07-16 01:22:57 +0200

    Arreglar problemas de despliegue

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: true,
     port: 5173
   },
   build: {
+    outDir: 'dist',
     target: 'es2015',
     cssTarget: 'chrome80',
     rollupOptions: {
